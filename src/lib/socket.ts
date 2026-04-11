d6cddf9d7eba969b899053c9413bc2f7b58f0c38
Tarun Wasnik <you@example.com>
2026-04-11 10:50:33 +0530

    fix:onboarding and user profile ui, frontend and backend

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -3,7 +3,7 @@
 import { io, Socket } from "socket.io-client";
 
 const SOCKET_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  import.meta.env.VITE_API_BASE_URL;
 
 export const socket: Socket = io(SOCKET_URL, {
   withCredentials: true,
